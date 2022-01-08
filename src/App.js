250360ccad1d14618ad029bd6e9176712c62c2b0
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-01-07 23:08:47 -0300

    version 2

--- src/App.js
+++ src/App.js
@@ -13,11 +13,7 @@ function App() {
   const [previous, setPrevious] = useState('')
   const [current, setCurrent] = useState('0')
   const [operator, setOperator] = useState(Operator.nada)
-
-  const getPreviousOperand = ()=>{
-    if (!previous) return ''
-    return `${previous} ${operator}`
-  }
+  const [overwrite, setOverwrite] = useState(false)
 
   const handleClear = ()=>{
     setPrevious('')
@@ -30,13 +26,12 @@ function App() {
   }
 
   const handleNumber = (e)=>{
-    // TODO: Está feo
-    let nuevoNumero = (current + e.target.innerText).replace(/^0+/g, '');
-    nuevoNumero = nuevoNumero.startsWith('.') ? '0' + nuevoNumero : nuevoNumero
-    setCurrent(nuevoNumero || '0')
-
-    //const newNumber = current === '0' && e.target.innerText === '0' ? current : current + e.target.innerText
-    setCurrent(nuevoNumero)
+    if(overwrite){
+      setCurrent(e.target.innerText)
+      setOverwrite(false)
+    } else {
+      setCurrent(quitarCerosDeIzquierda(current + e.target.innerText))
+    }
   }
 
   const handleDot = ()=>{
@@ -46,6 +41,15 @@ function App() {
   }
   
   const handleOperator = (e)=>{
+    // Si están todos los datos cargados entonces resuelve
+    if (quitarCerosDeIzquierda(current) && previous && operator) {
+      const result = evaluate(Number(previous), Number(current), operator)
+      setPrevious(result)
+      setCurrent('')
+      setOperator(e.target.innerText)
+      return
+    }
+
     if(current === '0') return
     if (operator === '') {
       setPrevious(current)
@@ -55,50 +59,65 @@ function App() {
   }
 
   const handleEvaluate = ()=>{
-    let res
-    switch(operator) {
-      case Operator.suma:
-        res = Number(previous) + Number(current)
-        break
-      case Operator.resta:
-        res = Number(previous) - Number(current)
-        break
-      case Operator.multiplicacion:
-        res = Number(previous) * Number(current)
-        break
-      case Operator.division:
-        res = Number(previous) / Number(current)
-        break
-      default:
-        return
+    if (!quitarCerosDeIzquierda(current) || !previous || !operator) {
+      return
     }
-
+    const result = evaluate(Number(previous), Number(current), operator)
     // TODO: Debería ir en Current, hasta que tipee y pase al Previous, excepto la primera vez
-    setCurrent(res)
+    setCurrent(result)
     setPrevious('')
     setOperator(Operator.nada)
+    setOverwrite(true)
+  }
+
+  const evaluate = (operando1, operando2, operador)=>{
+    let res = 0
+    switch(operador) {
+      case Operator.suma:
+        res = operando1 + operando2
+        break
+      case Operator.resta:
+        res = operando1 - operando2
+        break
+      case Operator.multiplicacion:
+        res = operando1 * operando2
+        break
+      case Operator.division:
+        res = operando1 / operando2
+        break
+      default:
+        return undefined
+    }
+    return res
+  }
+
+  const quitarCerosDeIzquierda = (expresion)=>{
+    if (Number(expresion) === 0) return ''
+    let numero = String(expresion).replace(/^0+/g, '')
+    numero = numero.startsWith('.') ? '0' + numero : numero
+    return numero
   }
 
   return (
     <div className="calculadora-grid">
       <div className='output'>
-        <div className='previous-operand'>{getPreviousOperand()}</div>
+        <div className='previous-operand'>{previous} {operator}</div>
         <div className='current-operand'>{current}</div>
       </div>
       <button className='span-two' onClick={handleClear}>AC</button>
       <button onClick={handleDelete}>DEL</button>
       <button onClick={handleOperator}>÷</button>
-      <button onClick={handleNumber}>1</button>
-      <button onClick={handleNumber}>2</button>
-      <button onClick={handleNumber}>3</button>
+      <button onClick={handleNumber}>7</button>
+      <button onClick={handleNumber}>8</button>
+      <button onClick={handleNumber}>9</button>
       <button onClick={handleOperator}>*</button>
       <button onClick={handleNumber}>4</button>
       <button onClick={handleNumber}>5</button>
       <button onClick={handleNumber}>6</button>
       <button onClick={handleOperator}>+</button>
-      <button onClick={handleNumber}>7</button>
-      <button onClick={handleNumber}>8</button>
-      <button onClick={handleNumber}>9</button>
+      <button onClick={handleNumber}>1</button>
+      <button onClick={handleNumber}>2</button>
+      <button onClick={handleNumber}>3</button>
       <button onClick={handleOperator}>-</button>
       <button onClick={handleDot}>.</button>
       <button onClick={handleNumber}>0</button>
